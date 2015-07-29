ff8d6184b9b66bae88290b93e7ebbc3b7b2756a0
Justin Laxamana <justin.laxamana@realart.com>
2015-07-29 10:43:20 -0500

    Update with current local build

---
+++ Gruntfile.js
@@ -0,0 +1,129 @@
+module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	concat: {
+		options: {
+			separator: ';'
+		},
+		vendor: {
+			src: [
+				'js/vendor/underscore-min.js',
+			],
+			dest: 'js/plugins.js'
+		}
+	},
+    uglify: {
+	    options: {
+			compress: {
+				drop_console: false
+			}
+		},
+		global: {
+        	files: {
+			'js/plugins.min.js': ['js/plugins.js'],
+			'js/main.min.js': ['js/main.js']
+        }
+      }
+    },
+
+    sass: {
+      global: {
+        options: {
+          style: 'compressed',
+          require: 'susy'
+        },
+        files: {
+          'css/style-unprefixed.css': 'scss/style.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      global: {
+        src: 'css/style-unprefixed.css',
+        dest: 'css/style.css'
+      }
+    },
+
+    shell: {
+      jekyllServe: {
+        command: 'jekyll serve --baseurl='
+      },
+      jekyllBuild: {
+        command: 'jekyll build --config _config-dev.yml'
+      }
+    },
+   imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'img/'
+        }]
+      }
+    },
+    grunticon: {
+	  icons: {
+	    files: [{
+	      expand: true,
+	      cwd: 'img/svg/',
+	      src: ['*.svg'],
+	      dest: 'fallbacks/'
+	    }]
+	  }
+	},
+    watch: {
+      options: {
+        livereload: true
+      },
+      site: {
+        files: ['index.html', '_layouts/*.html', '_includes/*.html'],
+        tasks: ['shell:jekyllBuild']
+      },
+      js: {
+        files: ['js/*.js'],
+        tasks: ['uglify', 'shell:jekyllBuild']
+      },
+      css: {
+        files: ['scss/*.scss'],
+        tasks: ['sass', 'autoprefixer', 'shell:jekyllBuild']
+      },
+      svgIcons: {
+        files: ['img/svg/*.svg'],
+        tasks: ['svgstore', 'grunticon', 'shell:jekyllBuild']
+      },
+	  images: {
+		files: ['img/**/*.{png,jpg,gif}', 'img/*.{png,jpg,gif}'],
+		tasks: ['imagemin', 'shell:jekyllBuild'],
+		options: {
+			spawn: false,
+			}
+		}
+    },
+    svgstore: {
+      options: {
+        prefix : 'shape-',
+        cleanup: false,
+        svg: {
+          style: 'display: none;'
+        }
+      },
+      default: {
+        files: {
+          'svg-defs.svg': ['img/svg/*.svg']
+        }
+      }
+    }
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('serve', ['shell:jekyllServe']);
+  grunt.registerTask('default', ['sass', 'autoprefixer', 'svgstore', 'imagemin', 'shell:jekyllBuild', 'watch']);
+
+};
